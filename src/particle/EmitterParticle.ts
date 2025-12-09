57cd3fd932d3e68f89a9dc653edde8ccdf513b65
Daniel Pokladek <danielpokladek@proton.me>
2025-12-09 10:20:03 +0000

    adds interface for particle which will be used in future

--- src/particle/EmitterParticle.ts
+++ src/particle/EmitterParticle.ts
@@ -1,39 +1,50 @@
 import { Particle, Texture } from "pixi.js";
 
 /**
+ * Type defining the data stored in each particle.
+ */
+export type BaseParticleData = {
+  maxLifetime: number;
+  age: number;
+  agePercent: number;
+  oneOverLifetime: number;
+};
+
+/**
+ * Interface defining an emitter particle.
+ */
+export interface IEmitterParticle extends Particle {
+  data: BaseParticleData;
+
+  /**
+   * Resets the particle data.
+   */
+  reset(): void;
+}
+
+/**
  * Particle class used by the Emitter.
  */
-export class EmitterParticle extends Particle {
+export class EmitterParticle extends Particle implements IEmitterParticle {
   public data: BaseParticleData;
 
   constructor() {
     super(Texture.WHITE);
 
-    this.data = {
-      maxLifetime: 1,
-      age: 0,
-      agePercent: 0,
-      oneOverLifetime: 1,
-    };
+    this.data = {} as BaseParticleData;
+    this.reset();
   }
 
   /**
-   * Resets the particle data.
+   * @inheritdoc
    */
   public reset(): void {
+    this.anchorX = 0.5;
+    this.anchorY = 0.5;
+
     this.data.age = 0;
     this.data.agePercent = 0;
     this.data.maxLifetime = 0;
     this.data.oneOverLifetime = 0;
   }
 }
-
-/**
- * Type defining the data stored in each particle.
- */
-type BaseParticleData = {
-  maxLifetime: number;
-  age: number;
-  agePercent: number;
-  oneOverLifetime: number;
-};
