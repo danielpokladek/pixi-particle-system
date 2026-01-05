1427a6b478440d337330c858932bb1b919dc5f23
Daniel Pokladek <danielpokladek@proton.me>
2026-01-05 11:04:39 +0000

    rotation behavior unit tests

---
+++ library/tests/behavior/built-in/RotationBehavior.test.ts
@@ -0,0 +1,179 @@
+import { ParticleContainer } from "pixi.js";
+import { describe, expect, it, vi } from "vitest";
+import { Emitter, RotationBehavior } from "../../../src";
+
+vi.mock("pixi.js", () => {
+    class ParticleContainer {
+        constructor() {}
+    }
+
+    class Texture {
+        static EMPTY = new Texture("EMPTY");
+        static WHITE = new Texture("WHITE");
+
+        constructor(public readonly id: string = "TEX") {}
+    }
+
+    class Particle {
+        public texture: Texture;
+
+        public anchorX = 0;
+        public anchorY = 0;
+
+        public alpha = 1;
+
+        public scaleX = 1;
+        public scaleY = 1;
+
+        public rotation = 0;
+
+        constructor(texture: Texture) {
+            this.texture = texture;
+        }
+    }
+
+    return { ParticleContainer, Particle };
+});
+
+vi.mock("../../../src/Emitter", () => {
+    class Emitter {
+        private _initActive = false;
+        private _updateActive = false;
+
+        constructor() {}
+
+        public isBehaviorInitActive(): boolean {
+            return this._initActive;
+        }
+        public isBehaviorUpdateActive(): boolean {
+            return this._updateActive;
+        }
+
+        public addToActiveInitBehaviors(): void {
+            this._initActive = true;
+        }
+        public addToActiveUpdateBehaviors(): void {
+            this._updateActive = true;
+        }
+
+        public removeFromActiveInitBehaviors(): void {
+            this._initActive = false;
+        }
+        public removeFromActiveUpdateBehaviors(): void {
+            this._updateActive = false;
+        }
+    }
+
+    return { Emitter };
+});
+
+describe("AlphaBehavior config application/retrieving", () => {
+    const container = new ParticleContainer();
+    const emitter = new Emitter(container);
+
+    it("retrieves undefined by default", () => {
+        const behavior = new RotationBehavior(emitter);
+        const config = behavior.getConfig();
+
+        expect(config).toBeUndefined();
+    });
+
+    it("applies and retrieves static config", () => {
+        const behavior = new RotationBehavior(emitter);
+        behavior.applyConfig({ mode: "static", value: 0.5 });
+
+        const config = behavior.getConfig();
+        expect(config).toEqual({ mode: "static", value: 0.5 });
+
+        expect(emitter.isBehaviorInitActive(behavior)).toBe(true);
+        expect(emitter.isBehaviorUpdateActive(behavior)).toBe(false);
+    });
+
+    it("applies and retrieves list config", () => {
+        const behavior = new RotationBehavior(emitter);
+        behavior.applyConfig({
+            mode: "list",
+            listData: {
+                list: [
+                    { time: 0, value: 0 },
+                    { time: 0.5, value: 1 },
+                    { time: 1, value: 0 },
+                ],
+            },
+        });
+
+        const config = behavior.getConfig();
+        expect(config).toEqual({
+            mode: "list",
+            listData: {
+                list: [
+                    { time: 0, value: 0 },
+                    { time: 0.5, value: 1 },
+                    { time: 1, value: 0 },
+                ],
+            },
+        });
+
+        expect(emitter.isBehaviorInitActive(behavior)).toBe(true);
+        expect(emitter.isBehaviorUpdateActive(behavior)).toBe(true);
+    });
+
+    it("applies and retrieves random config", () => {
+        const behavior = new RotationBehavior(emitter);
+        behavior.applyConfig({
+            mode: "random",
+            listData: {
+                list: [
+                    { time: 0, value: 0 },
+                    { time: 0.5, value: 1 },
+                    { time: 1, value: 0 },
+                ],
+            },
+        });
+
+        const config = behavior.getConfig();
+        expect(config).toEqual({
+            mode: "random",
+            listData: {
+                list: [
+                    { time: 0, value: 0 },
+                    { time: 0.5, value: 1 },
+                    { time: 1, value: 0 },
+                ],
+            },
+        });
+
+        expect(emitter.isBehaviorInitActive(behavior)).toBe(true);
+        expect(emitter.isBehaviorUpdateActive(behavior)).toBe(false);
+    });
+
+    it("applies and retrieves direction config", () => {
+        const behavior = new RotationBehavior(emitter);
+        behavior.applyConfig({ mode: "direction" });
+
+        const config = behavior.getConfig();
+        expect(config).toEqual({ mode: "direction" });
+
+        expect(emitter.isBehaviorInitActive(behavior)).toBe(true);
+        expect(emitter.isBehaviorUpdateActive(behavior)).toBe(false);
+    });
+
+    it("applies and retrieves acceleration config", () => {
+        const behavior = new RotationBehavior(emitter);
+        behavior.applyConfig({
+            mode: "acceleration",
+            startRotation: 0.5,
+            acceleration: 0.1,
+        });
+
+        const config = behavior.getConfig();
+        expect(config).toEqual({
+            mode: "acceleration",
+            startRotation: 0.5,
+            acceleration: 0.1,
+        });
+
+        expect(emitter.isBehaviorInitActive(behavior)).toBe(true);
+        expect(emitter.isBehaviorUpdateActive(behavior)).toBe(true);
+    });
+});
